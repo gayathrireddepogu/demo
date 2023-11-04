de35968e9bcba0677bf977a3f99c9370e21fe1a4
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-11-04 17:26:16 +0530

    first commit

---
+++ Task-3/Narrowingtypecastingtypes.java
@@ -0,0 +1,16 @@
+package Gayu;
+
+public class Narrowingtypecastingtypes {
+
+	public static void main(String[] args) { 
+		double d = 222.333;   
+		long l = (long)d;  
+		int i = (int)l;  
+		System.out.println("Before conversion: "+d);   
+		System.out.println("After conversion into long type: "+l);    
+		System.out.println("After conversion into int type: "+i);  
+		}  
+		
+
+	}
+
---
+++ Task-3/Wideingtypecasting.java
@@ -0,0 +1,17 @@
+package Gayu;
+
+public class Wideingtypecasting {
+
+	public static void main(String[] args) {
+		int x = 66;    
+		long y = x;    
+		float z = y;  
+		System.out.println("Before conversion, int value "+x);  
+		System.out.println("After conversion, long value "+y);  
+		System.out.println("After conversion, float value "+z);  
+		}  
+		
+
+	}
+
+
