f925fcda3d7e0d81b699aaccdfba6e4f937e4437
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-11-06 09:49:40 +0530

    first commit

---
+++ Task-4/Teranyoprators.java
@@ -0,0 +1,17 @@
+package Gayu;
+
+public class Teranyoprators {
+
+	public static void main(String[] args) {
+		int x, y;  
+		x = 250;  
+		y = (x == 5) ? 6: 90;  
+		System.out.println(y);  
+		y = (x == 250) ? 6: 90;  
+		System.out.println( y);  
+		}  
+		 
+
+	}
+
+
