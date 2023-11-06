8facbcf7b0a939158944c4ef137cedc7ed5121ed
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-11-06 09:45:59 +0530

    first commit

---
+++ Task-5/Ifelse.java
@@ -0,0 +1,17 @@
+package Gayu;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		int n=3600;  
+	    
+	    if(n%2==0){  
+	        System.out.println("even number");  
+	    }else{  
+	        System.out.println("odd number");  
+
+	}
+
+  }
+	
+}
