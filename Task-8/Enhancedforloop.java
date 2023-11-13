c251fc133c86268a125252721d652db3fb6c571c
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-11-13 09:08:26 +0530

    first commit

---
+++ Task-8/Enhancedforloop.java
@@ -0,0 +1,17 @@
+package Task8;
+
+public class Enhancedforloop {
+
+	public static void main(String[] args) {
+		int arr[]={12,13,14,44};  
+		   int total=0;  
+		   for(int i:arr){  
+		     total=total+i;  
+		   }  
+		  System.out.println("Total: "+total);  
+		 }   
+		
+
+	}
+
+
