0b8af551593b9a01a3e9dc735f019052867d2ffe
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-11-22 18:08:46 +0530

    first commit

---
+++ Task-17/Array.java
@@ -0,0 +1,20 @@
+package Task17;
+
+import java.util.*;
+
+class Array{
+
+ public static void main(String[] args)
+ {
+
+     int arr1[] = { 1, 2, 3 };
+     int arr2[] = { 1, 2, 3 };
+
+     if (arr1 == arr2)
+
+         System.out.println("Same");
+     else
+
+         System.out.println("Not same");
+ }
+}
---
+++ Task-17/package-info.java
@@ -0,0 +1,1 @@
+package Task17;
