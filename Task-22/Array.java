0d5a3e69bf01cb86b222d4f1a01387696f60f50b
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-11-30 17:54:23 +0530

    first commit

---
+++ Task-22/Array.java
@@ -0,0 +1,16 @@
+package Task22;
+
+public class Array {
+    public static void main(String[] args) {
+        
+        double[] doubleArray = new double[3];
+
+        doubleArray[0] = 3.14;
+        doubleArray[1] = 2.71;
+        doubleArray[2] = 1.0;
+
+        System.out.println("Element at index 0: " + doubleArray[0]);
+        System.out.println("Element at index 1: " + doubleArray[1]);
+        System.out.println("Element at index 2: " + doubleArray[2]);
+    }
+}
