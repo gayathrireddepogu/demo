dab32988aa1c9f5278653405ca14e741bcaa0b36
gayathri <135616201+gayathrireddepogu@users.noreply.github.com>
2023-12-06 17:55:45 +0530

    first commit

---
+++ Task-26/Wrapperclass.java
@@ -0,0 +1,11 @@
+package Task26;
+
+public class Wrapperclass{  
+public static void main(String args[]){  
+ 
+int a=600;  
+Integer i=Integer.valueOf(a);  
+Integer j=a;
+  
+System.out.println(a+" "+i+" "+j);  
+}}  
